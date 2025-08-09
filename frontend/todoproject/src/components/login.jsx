4c3a0220b349845fefa7f1a1ef014665ecf22c29
Nitinsaini1515 <gamern662@gmail.com>
2025-08-09 12:22:45 +0530

    Implement user login and registration functionality with Axios integration; add form validation and navigation on successful login/register

--- frontend/todoproject/src/components/login.jsx
+++ frontend/todoproject/src/components/login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
-
+import axios from 'axios'
+import React, { useState } from 'react'
+// import { Navigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 const Login = () => {
+  const[username,setUsername] = useState("")
+  const[email,setEmail] = useState("")
+  const[password,setPassword] = useState("")
+  const [message,setMessage] = useState("")
+const navigate = useNavigate()
+  const loginUser = async(e)=>{
+e.preventDefault()
+try {
+  
+  const response = await axios.post("/api/v1/user/login",{
+   username,email,password 
+  },{
+    withCredentials:true,
+    headers:{
+      "Content-Type":"application/json"
+    }
+  })
+  navigate("/home")
+} catch (error) {
+ if(error.response){
+  setMessage(error.response.data.message)
+}}
+
+    
+  }
   return (
     <div>
-      
+<div>
+  <h1>Login</h1>
+  <form method='post' onSubmit={loginUser}>
+  <label htmlFor="username">Username:</label>
+<input type="text" id="username" value={username} onChange={(e)=>setUsername(e.target.value)} />
+  <label htmlFor="email">Email:</label>
+<input type="text" id="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+  <label htmlFor="password">Password</label>
+<input type="password" id="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+<button type='submit'>Login
+</button>
+  </form>
+</div>
+
     </div>
   )
 }
--- frontend/todoproject/src/components/register.jsx
+++ frontend/todoproject/src/components/register.jsx
@@ -1,41 +1,55 @@
 import React, { useState } from "react";
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Register =()=>{
+
+  const navigate = useNavigate()
 const [username,setUsername] = useState("")
 const[password,setPassword] = useState("")
 const [email,setEmail] = useState("")
 const [fullname,setFullname] = useState("")
 const [message,setMessage] = useState("")
-const registerUser = async()=>{
-  if((username&&password&&email&&fullname).trim()== ""){
+const registerUser = async(e)=>{
+  e.preventDefault()
+  if([username,email,password,fullname].some((field=>!field.trim()))){
     setMessage("All fields are required")
     return;
   }
   try {
     
     const response = await axios.post("/api/v1/user/register",
-      {fullname:fullname},
-      {username:username},
-      {email:email},
-      {password:password},{
+      {fullname,username,email,password},{
             withCredentials:true,
       headers:{
 "Content-Type":"application/json"}
       }
     );
     setMessage("User is registerd successfully")
+    navigate("/login")
   } catch (error) {
     setMessage("There is an error in register")
   }
 }
 
 return(
-  <>
-  <form action="/login" method="post">
-  
+  <div>
+  <div>
+    <h2>Sign up</h2>
+  <form onSubmit={registerUser} method="post">
+  <label htmlFor="fullname">Fullname</label>
+  <input type="text" required id="fullname" value={fullname} onChange={(e)=>setFullname(e.target.value)}/>
+  <label htmlFor="username">username</label>
+  <input type="text" required id="username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
+  <label htmlFor="email">email</label>
+  <input type="text" required id="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+  <label htmlFor="password">password</label>
+  <input type="password" required id="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+  <button type="submit">Register</button>
   </form>
-  </>
+
+  </div>
+  </div>
 )
 }
 export default Register
